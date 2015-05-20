df28ea23579306e0f058c9dc6e1646a2a96baecb
ocdnecro <blancos2004@yahoo.com.mx>
2015-05-20 13:25:28 -0500

    Frontend con Boton de logIn

--- Documents/NetBeansProjects/appHospital/public_html/js/AngularControllers/app.js
+++ Documents/NetBeansProjects/appHospital/public_html/js/AngularControllers/app.js
@@ -7,7 +7,7 @@
 
 (function(){var aplicacionHospital= angular.module('aplicacionHospital',[]);
     
-    
+    var x_rest_user;
                    
     aplicacionHospital.directive('toolbar', function(){
         return{
@@ -47,11 +47,23 @@
                    self.competitors = [{id: "54FD3C141AEF28D54A9B2F3D", fecha: "201001011213", cedula: 1, localizacion: "Frente", alivio: 0.0, intensidad: 0.0, nivelDolor: 10.0}];
                    console.log("va a mirar paa"+ self.pa.id);
                    var head = {'Autorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InVzdWFyaW8xIiwicGFzc3dvcmQiOiJVc3VhcmlvMSIsImVtYWlsIjoidXN1YXJpbzFAY2xpbmljYS5jb20iLCJuYW1lIjoiVXN1YXJpbzEgQXBlbGxpZG9Vc3VhcmlvMSIsImxldmVsQWNjZXNzIjoiVXNlciJ9.LYgLvJ79kenLFlyfaRVib3_aZ56iiD59oqCksUZMC-w'};
-                   $http.get('http://172.24.99.155:8080/Doctor/consultarEpisodiosPaciente/11/'+id, {    headers: {'x_rest_user': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InVzdWFyaW8xIiwicGFzc3dvcmQiOiJVc3VhcmlvMSIsImVtYWlsIjoidXN1YXJpbzFAY2xpbmljYS5jb20iLCJuYW1lIjoiVXN1YXJpbzEgQXBlbGxpZG9Vc3VhcmlvMSIsImxldmVsQWNjZXNzIjoiVXNlciJ9.LYgLvJ79kenLFlyfaRVib3_aZ56iiD59oqCksUZMC-w'}}).success(function(data){
+                   $http.get('http://172.24.99.153:8080/Doctor/consultarEpisodiosPaciente/11/'+id, {    headers: {'x_rest_user': x_rest_user}}).success(function(data){
                         self.competitors=data;
-                        this.nohash = this.competitors.
+                        self.longitud=data.length; 
+                        hasi=data[data.length-1].id;
+                        dto=data[data.length-1].localizacion;
+                        accion=0;
+                        console.log(data);
+                        console.log(self.longitud);
+                        console.log(data[data.length-1]);
+                        console.log("el hash "+hasi);
+                        
+                        mauricio= verifica_md5(hasi, dto);
+                        console.log("el resultado es "+mauricio);
+                        
+                        self.nohash = this.competitors.$last.id;
                          toolbar.occ = "active";
-                         console.log("va a mirar pa"+ self.pa.id);
+                         console.log("va a mirar pa"+ self.nohash.id);
                     });
                     return self.competitors;
                 };
@@ -148,19 +160,22 @@
             templateUrl:'partials/log-in.html',
             controller: ['$http',function($http){
                 var self=this;
-                self.competitors=[];
-                self.competitor={};
-                self.var = 0;
-                this.doLogin=function(ced, id){
-                    $http.post('http://172.24.99.167:8080/auth/login').success(function(data){
-                        self.competitors=data;
+                var vm = this;
+                vm.fdatos = {};
+                console.log(self.fdatos);
+                var dataObject = {username:vm.fdatos.username,password:vm.fdatos.password, user:vm.fdatos.user};
+                x_rest_user = "";
+                vm.enviar=function(){
+                    dataObject = {username:vm.fdatos.username,password:vm.fdatos.password, user:vm.fdatos.user};
+                    console.log(dataObject.user);
+                    $http.post('http://localhost:8080/auth/login',vm.fdatos).success(function(data){
+                        
+                        x_rest_user=data;
+                        console.log(data);
                          toolbar.occ = "active";
                     });
-                   return self.competitors;
+                   return self.x_rest_user;
                 };
-                
-                
-                
             }],
             controllerAs:'loginCtrl'
         };
